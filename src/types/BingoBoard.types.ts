0c403aae9625fa9dc798d3f252c0dd64b67a244e
JDRL555 <joshuleal3@gmail.com>
2025-03-07 20:33:33 -0400

    Revert "feat: migrate the project to NextJS and implemented the winner search"
    This reverts commit 457a6bd626d38e8e31600e0c016151b95f0e685b.
    
    Apply revert

--- src/types/BingoBoard.types.ts
+++
@@ -1,8 +0,0 @@
-import { CSSProperties } from 'react'
-
-export interface BingoCell {
-  number: number,
-  colors: CSSProperties["color"][],
-  clicked: boolean,
-  letter: "B" | "I" | "N" | "G" | "O" | ""
-}
